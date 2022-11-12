39412492f18443c48476e2caf432f138314634e2
Mouad Errahmouni <mouad@tousfacteurs.com>
2022-11-12 23:11:35 +0100

    fix config place

--- src/DependencyInjection/MouadCoreBundleExtension.php
+++ src/DependencyInjection/MouadCoreBundleExtension.php
@@ -11,7 +11,7 @@ class MouadCoreBundleExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
 
         $configuration = $this->getConfiguration($configs, $container);
